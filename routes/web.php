bb21a98ab98062e9cab7ff89cb1f7bca41b3a244
KhomarulH <2317051029@students.unila.ac.id>
2025-10-09 18:58:45 +0700

    Selesai Langkah Praktikum Modul 5

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
 use App\http\Controllers\UserController;
+use App\Http\Controllers\MataKuliahController;
 
 
 /*
@@ -26,5 +27,8 @@ Route::get('/user',[UserController::class,'index']);
 Route::get('/user/create',[UserController::class,'Create']);
 Route::post('/user',[UserController::class,'store']);
 Route::post('/user', [UserController::class, 'store'])->name('users.store');
+Route::get('/matakuliah', [MataKuliahController::class, 'index']);
+Route::get('/matakuliah/create', [MataKuliahController::class, 'create']) ->name('matakuliah.create');
+Route::post('/matakuliah', [MataKuliahController::class, 'store']) ->name('matakuliah.store');
 
 
